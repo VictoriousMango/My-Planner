d1ce78f7217d893ce45af3f3e60c53f1aa600a91
VictoriousMango <additya.mishra20@st.niituniversity.in>
2024-08-22 10:42:35 +0530

    Updating Reading Tab

--- assets/Read.js
+++ assets/Read.js
@@ -19,46 +19,33 @@ document.getElementById('fileInput').addEventListener('change', function (event)
 
             // Loop through each line and print it
             let accordionIndex = 0;
-            let accordionBody = '';
-
-            for (let i = 0; i < lines.length; i++) {
-                if (lines[i].includes("###")) {
-                    // Close the previous accordion item if there is one
-                    if (accordionBody !== '') {
-                        contentElement.innerHTML += `
-                                <div id="collapse${accordionIndex}" class="accordion-collapse collapse" aria-labelledby="heading${accordionIndex}" data-parent="#accordionExample">
-                                    <div class="accordion-body">${accordionBody}</div>
-                                </div>
-                            `;
-                        accordionBody = '';
-                    }
-
-                    accordionIndex++;
-                    const header = lines[i].replace("###", "").trim();
-                    contentElement.innerHTML += `
-                            <div class="accordion-item">
-                                <h2 class="accordion-header">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${accordionIndex}" aria-expanded="false" aria-controls="#collapse${accordionIndex}">
-                                        ${header}
-                                    </button>
-                                </h2>
-                        `;
-                } else {
-                    // Add the line to the current accordion body
-                    accordionBody += `<p>${lines[i]}</p>`;
+            let ContentHeader = "";
+            var ContentMap = {};
+            for (let i = 0; i < lines.length; i++){
+                if(lines[i].includes("###")){
+                    ContentHeader = lines[i].replace("### ", "");
+                    console.log("Header:", lines[i]);
+                    ContentBody = "";
                 }
-            }
+                else{
+                    ContentMap[ContentHeader] += `<p>${lines[i]}</p>`;
+                    console.log("Body:", lines[i]);
+                }
 
-            // Add the last accordion body content if any
-            if (accordionBody !== '') {
+            }
+            for(i in ContentMap){
                 contentElement.innerHTML += `
-                            <div id="collapse${accordionIndex}" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    ${accordionBody}
-                                </div>
-                            </div>
+                <div class="col">
+                    <div class="card width: 18rem;">
+                        <div class="card-header">
+                            ${i}
                         </div>
-                    `;
+                        <div class="card-body">
+                            <p>${ContentMap[i]}<p>
+                        </div>
+                    </div>
+                </div>
+                `;
             }
         };
         reader.readAsText(file);
